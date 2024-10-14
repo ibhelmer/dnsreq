eb85859290a0a75253fd6a3e74f3cda4c5be9b80
Ib Helmer Nielsen <ihn@ihn.dk>
2024-10-14 14:33:52 +0200

    Første upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from scapy.all import *
+
+# Sæt domænenavnet, som du ønsker at lave DNS-forespørgsel på
+domain_name = "ibhelmer.dk"
+
+# Opret en DNS-forespørgselspakke
+dns_request = IP(dst="8.8.8.8") / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname=domain_name))
+
+# Send forespørgslen og modtag svaret
+dns_response = sr1(dns_request, verbose=0)
+
+# Udskriv DNS-svaret
+if dns_response and dns_response.haslayer(DNS):
+    for i in range(dns_response[DNS].ancount):
+        print(dns_response[DNS].an[i].rdata)
+else:
+    print("Ingen DNS-svar modtaget.")
